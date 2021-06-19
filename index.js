3d441b56786bcdc0002f5a49ad84273795d319db
Mochammad Fadholi <mochammad.fadholi.st@gmail.com>
2021-06-20 01:51:48 +0700

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+fs.readFile('./data.text', 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  var result = new Date();
+
+  fs.writeFile('./data.text', result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
